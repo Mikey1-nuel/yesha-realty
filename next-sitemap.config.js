6815da76ea7168a56792960f5eae0e8c49982190
Mikey1-nuel <emmanuelnwoye12@gmail.com>
2025-09-07 06:06:46 +0100

    made some changes to next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://yesha-realty.vercel.app/',
-  generateRobotsTxt: true, // ✅ also creates robots.txt
+  generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
   priority: 0.7,
+  exclude: [
+    '/components/*',
+    '/pages/*',
+  ],
 };
+
